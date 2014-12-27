94bfbd62fcad5f36ca10d4b4645ae1c333a7c907
Jayaneetha <>
2014-12-27 11:41:09 +0530

    Initialize CI - FlatUI

---
+++ application/views/partial/footer.php
@@ -0,0 +1,17 @@
+<div class="footer">
+    <p>Footer Text</p>
+</div>
+
+<!-- Load JS here for greater good =============================-->
+    <script src="<?php base_url('assests/js/jquery-1.8.3.min.js');?>"></script>
+    <script src="<?php base_url('assests/js/jquery-ui-1.10.3.custom.min.js');?>"></script>
+    <script src="<?php base_url('assests/js/jquery.ui.touch-punch.min.js');?>"></script>
+    <script src="<?php base_url('assests/js/bootstrap.min.js');?>"></script>
+    <script src="<?php base_url('assests/js/bootstrap-select.js');?>"></script>
+    <script src="<?php base_url('assests/js/bootstrap-switch.js');?>"></script>
+    <script src="<?php base_url('assests/js/flatui-checkbox.js');?>"></script>
+    <script src="<?php base_url('assests/js/flatui-radio.js');?>"></script>
+    <script src="<?php base_url('assests/js/jquery.tagsinput.js');?>"></script>
+    <script src="<?php base_url('assests/js/jquery.placeholder.js');?>"></script>
+  </body>
+</html>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<?php $this->load->view('partial/header'); ?>
+</head>
+<body>
+
+<div id="container">
+    <h1 class="h1 text-center">Welcome to CodeIgniter - FlatUI !</h1>
+
+	<div id="body">
+            <p class="text-center text-info">The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/welcome.php</code>
+
+	</div>
+
+</div>
+
+<?php $this->load->view('partial/footer'); ?>
