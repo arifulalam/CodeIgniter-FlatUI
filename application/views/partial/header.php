7ff524670b401760911cf38ad799f49721077561
Jayaneetha <>
2014-12-27 11:41:09 +0530

    Initialize CI - FlatUI

---
+++ application/views/partial/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>CodeIgnitor - Flat UI</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    
+    <link href="<?php echo base_url('assests/bootstrap/css/bootstrap.css'); ?>" rel="stylesheet">
+
+    <!-- Loading Flat UI -->
+    <link href="<?php echo base_url('assests/css/flat-ui.css'); ?>" rel="stylesheet">
+
+    <link rel="shortcut icon" href="<?php echo base_url('assests/images/favicon.ico'); ?>">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
---
+++ application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<?php $this->load->view('partial/header'); ?>
+</head>
+<body>
+
+<div id="container">
+    <h1 class="h1 text-center">Welcome to CodeIgniter - FlatUI !</h1>
+
+	<div id="body">
+            <p class="text-info">The page you are looking at is being generated dynamically by CodeIgniter.</p>
+            <div class="alert alert-info">
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/welcome.php</code>
+            </div>
+	</div>
+
+</div>
+
+<?php $this->load->view('partial/footer'); ?>
